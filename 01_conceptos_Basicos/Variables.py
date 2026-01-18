61d8dc016d5890e0f7837be7e27f5c04ca5fea9c
Joel Alamillo <alamillojuarez@gmail.com>
2026-01-17 19:33:41 -0600

    Conceptos básicos

---
+++ 01_conceptos_Basicos/Variables.py
@@ -0,0 +1,14 @@
+#Programa: Información personal
+
+#Guardar los datos en variables
+nombre = "Joel"
+edad = 22
+pais = "México"
+frase_favorita = "echale ganas"
+
+#Mostar información en consola
+print("Nombre: ", nombre)
+print("Edad: ", edad)
+print("País: ", pais)
+print("Frase favorita: ", frase_favorita)
+
