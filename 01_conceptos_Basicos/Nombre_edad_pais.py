df237c04b36512ba61788a47a631bb994d3ee354
Joel Alamillo <alamillojuarez@gmail.com>
2026-01-17 19:20:26 -0600

    Programa de presentación personal

---
+++ 01_conceptos_Basicos/Nombre_edad_pais.py
@@ -0,0 +1,11 @@
+#Este programa imprime tu nombre tu edad y tu país de origen
+
+#Mostremos el nombre
+print("Nombre: Joel Alamillo Juárez")
+
+#Mostremos la edad
+print("Edad: 38 años")
+
+#mostramos el país
+# print("País: México")
+
