be4339f129d8c4f466d0f488ac65c6db483349ce
Joel Alamillo <alamillojuarez@gmail.com>
2026-01-17 19:50:22 -0600

    Programa que modifica las variables

---
+++ 01_conceptos_Basicos/modificar_variables.py
@@ -0,0 +1,24 @@
+#Programa: Cambio de identidad del superheroe
+
+#Asignar valores iniciales
+nombre = "El programador nocturno"
+nivel_energia = 85
+tiene_capa = False
+
+#encabezado
+print ("=== El superheroe ===")
+
+print ("nombre: ", nombre)
+print ("nivel de energia: ", nivel_energia)
+print ("tiene capa: ", tiene_capa)
+
+#Modificando los variables
+nombre = "El programador diurno"
+nivel_energia = 50
+tiene_capa = True
+
+
+#imprimiendo datos modificados
+print ("nombre: ", nombre)
+print ("nivel de energia: ", nivel_energia)
+print ("tiene capa: ", tiene_capa)
