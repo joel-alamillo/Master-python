8f2b2831727704804d74790f213ae2fe4f743296
Joel Alamillo <alamillojuarez@gmail.com>
2026-01-17 19:44:22 -0600

    Programa de tipos de datos

---
+++ 01_conceptos_Basicos/tipos_datos.py
@@ -0,0 +1,36 @@
+#Programa: tipos Ficha secreta del super heroe
+
+#tipos de datos en acción
+#str (texto)
+nombre = "Capitan Python"
+poder_principal = "Controlar los bugs con la mente"
+
+# int (numero entero)
+edad = 28
+misiones_completas = 34
+
+# float (numero decimal)
+nivel_energia = 99.8
+
+#bool 
+tiene_capa = True 
+es_villano = False
+
+# NoneType (sin valor)
+compania_actual = None
+
+#Imprimir el encabezado
+print ("=== FICHA SECRETA DEL SUPERHEROE ===")
+
+#imprimir datos
+print ("Nombre de super heroe: ", nombre)
+print ("Poder principal: ", poder_principal)
+print ("Edad: ", edad)
+print ("Misiones completadas: ", misiones_completas)
+print ("Nivel de energía: ", nivel_energia)
+print ("Tiene capa: ", tiene_capa)
+print ("Es villano: ", es_villano)
+
+#imprimir la variable de los datos
+print (type(es_villano))
+print (type(compania_actual))
